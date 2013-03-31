d90172548008044c0543eee74cfe9cf9a2d21475
Elias Hassen <ehassen@gmail.com>
2013-03-30 22:01:32 -0700

    checking in more files

---
+++ Scripts/comparison.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="jquery-1.9.1.intellisense.js" />
+var c = {
+    startup: {
+        begin: function () {
+            var ctx = c.startup;
+            ctx.alternatingTbleRowstyles();
+            ctx.setupAudioEffectsForHyperlinks();
+        },
+        alternatingTbleRowstyles: function () {
+            $('.comparisonTable tr:even').addClass('even');
+            $('.comparisonTable tr:odd').addClass('odd');
+        },
+        setupAudioEffectsForHyperlinks:function(){
+            
+        }
+    }
+    
+};
+
+$(c.startup.begin);
